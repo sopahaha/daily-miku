4820aa9affb2f3bdda20af1f9feb6258f28b5ba8
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2025-12-05 17:13:05 -0300

    feat: add daily messages system

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import smtplib
 import ssl
 import csv
 import os
+import json
+from time import sleep
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 
 load_dotenv()
+
+MESSAGES_FILE = "messages.json"
+
+with open(MESSAGES_FILE, "r") as f:
+    messages_json = json.load(f)
+
+day_index = messages_json['day_index']
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -14,22 +23,17 @@ miku_email = os.getenv('EMAIL_ADDR')
 receiver_email = "vitorcesarino1@gmail.com"
 miku_password = os.getenv('EMAIL_PASSWORD')
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "Daily miku for you"
-message["From"] = miku_email
 
 html = """\
 <html>
   <body>
     <p>Hi, {name}<br>
-      Check out the new miku on your mail:</p>
-    <img src="https://c.tenor.com/rUkr3PJaIHAAAAAd/tenor.gif"></img>
+      {text}</p>
+    <img src="{image}"></img>
   </body>
 </html>
 """
 
-convertedHtml = MIMEText(html, "html")
-message.attach(convertedHtml)
 
 context = ssl.create_default_context()
 
@@ -37,16 +41,33 @@ try:
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         print("Authenticating...")
         server.login(miku_email, miku_password)
+
         print("Sending mails...")
         with open("emails.csv") as file:
             reader = csv.reader(file)
             next(reader)
-            for email, name in reader:
-                server.sendmail(miku_email, email,
-                                message.as_string().format(name=name))
-                print(f"Sent to {name}")
 
+            for email, name in reader:
+                message = MIMEMultipart("alternative")
+                message["Subject"] = "Daily miku for you"
+                message["From"] = miku_email
+                formatedHtml = html.format(
+                    name=name,
+                    text=messages_json["daily_messages"][day_index],
+                    image=messages_json["daily_images"][day_index])
+                convertedHtml = MIMEText(formatedHtml, "html")
+                message.attach(formatedHtml)
+
+                print(formatedHtml)
+
+                # server.sendmail(miku_email, email,
+                #                 message.as_string())
+                # sleep(4)
+                print(f"Sent to {name}")
         print("Sent")
+        messages_json["day_index"] += 1
+        with open(MESSAGES_FILE, "w") as f:
+            json.dump(messages_json, f)
         server.close()
 except Exception as e:
     print(f"error: {e}")
